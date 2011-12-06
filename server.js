ea07f9a10a51b915a200c7571881a0891a9a32cc
Howard Tyson <howard@howardtyson.com>
2011-12-06 12:18:18 -0500

    Successfully serving folder data on the REST service

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var settings = {
   host : '127.0.0.1'
 };
 
+var GIT_DIRECTORY = 16384;
+var GIT_SUBMODULE = 160000;
+
 /**
  * Sends a 404 message.
  */
@@ -21,16 +24,19 @@ var getObjectCallback = function(request, response) {
   var repoPath = request.query.path;
   var objectPath = request.query.objectPath;
   var ref = request.params.ref || '';
-  var parts = objectPath.split('/');
+  var pathParts = objectPath.split('/');
+  // Asynchronously make all the disk calls to find our top level tree.
   gitteh.openRepository(repoPath, function(error, repository) {
+    // TODO: make this work with something other than HEAD.
     repository.getReference("HEAD", function(error, ref) {
       ref.resolve(function(error, ref) {
         repository.getCommit(ref.target, function(error, commit) {
-          serveTreeData(repository, commit.tree, response);
-          /*
+          // TODO: This was useful here for testing but should really be served
+          // called by findObjectFromPath().
+          //serveTreeData(repository, commit.tree, response);
           repository.getTree(commit.tree, function(error, tree) {
+            findObjectFromPath(pathParts, tree, repository, request, response);
           });
-          */
         });
       });
     });
@@ -38,69 +44,90 @@ var getObjectCallback = function(request, response) {
 }
 
 /**
- * @param parts
+ * @param pathParts
  *   A linear array of path segments.
  * @param tree
  *   A gitteh tree object to search within.
  * @param repository
  *   The repository from which to fetch to objects.
- * @param next
- *   The function to call when the function completes.
- *   Defaults to this function itself.
+ * @param request
+ *   A response object for this request.
+ * @param response
+ *   A request object for this request.
  */
-var findObjectFromPath = function(parts, tree, repository, next) {
-  // Determine if we 
-  if (lastPart) {
-    getObjectHash(parts, repository);
+var findObjectFromPath = function(pathParts, tree, repository, request, response) {
+  // Determine if we have the last part.
+  var currentPart = pathParts.shift();
+  // If there are no remaining path parts, this is the final object.
+  var finalObject = (pathParts.length == 0);
+  var matchItem = false;
+  var isDirectory = false;
+  for (var i in tree.entries) {
+    var item = tree.entries[i];
+    if (item.name == currentPart) {
+      matchItem = item;
+      // TODO: create constants for magic numbers.
+      if (item.attributes == GIT_DIRECTORY) {
+        isDirectory = true;
+      }
+      break;
+    }
   }
-  var someError = false;
-  if (someError) {
-    send404();
+  if (matchItem == false) {
+    console.log(currentPart);
+    send404(request, response);
+  }
+  // No match was found, send an error.
+  else {
+    if (isDirectory) {
+      repository.getTree(item.id, function(error, tree) {
+        if (finalObject) {
+          serveTreeData(tree, response);
+        }
+        else {
+          findObjectFromPath(pathParts, tree, repository, request, response);
+        }
+      });
+    }
   }
 }
 
-var checkObject = function(item, repository) {
-
+/**
+ * Serve the raw blob object.
+ */
+var serveObjectData = function (repository, hash) {
 }
 
 /**
  * Serve the data from a tree.
  */
-var serveTreeData = function(repository, hash, response) {
+var serveTreeData = function(tree, response) {
   var treeData = {};
   // Asynchronously load a tree and return its data.
-  var output = repository.getTree(hash, function (error, tree) {
-    treeData.type = 'directory';
-    treeData.contents = {};
-    for (var i in tree.entries) {
-      var item = tree.entries[i];
-      var isSubmodule = false;
-      var isDirectory = false;
-      switch (item.attributes) {
-        case 16383:
-          isDirectory = true;
-          break;
-        case 160000:
-          isSubmodule = true;
-          break;
-      }
-      if (item.attributes == 16383) {
-        var isDirectory = true;
-      }
-      else {
-        var isDirectory = false;
-      }
-      treeData.contents[item.name] = {
-        name : item.name,
-        attributes : item.attributes,
-        directory : isDirectory,
-        mode : item.attributes,
-        submodule : isSubmodule,
-        object_id : item.id
-      };
+  treeData.type = 'directory';
+  treeData.contents = {};
+  for (var i in tree.entries) {
+    var item = tree.entries[i];
+    var isSubmodule = false;
+    var isDirectory = false;
+    switch (item.attributes) {
+      case GIT_DIRECTORY:
+        isDirectory = true;
+        break;
+      case GIT_SUBMODULE:
+        isSubmodule = true;
+        break;
     }
-    response.send(treeData);
-  });
+    treeData.contents[item.name] = {
+      name : item.name,
+      attributes : item.attributes,
+      directory : isDirectory,
+      mode : item.attributes,
+      submodule : isSubmodule,
+      object_id : item.id
+    };
+  }
+  response.send(treeData);
 }
 
 var getObject = function (repository, path) {
@@ -109,6 +136,7 @@ var getObject = function (repository, path) {
 
 var getCommit = function (repository, path) {
   /*
+  // TODO: Figure out the best way to get a diff.
   exec("git show " + commit.id, function (error, stdout, stderr) {
     response.seld(stdout);
   });
